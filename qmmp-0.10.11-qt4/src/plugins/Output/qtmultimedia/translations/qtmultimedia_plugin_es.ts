272eb1ea1e17aaa3784c283e057a693a4343fbae
Greedysky <greedysky@163.com>
2017-10-23 13:58:16 +0800

    Update

---
+++ qmmp-0.10.11-qt4/src/plugins/Output/qtmultimedia/translations/qtmultimedia_plugin_es.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>OutputQtMultimediaFactory</name>
+    <message>
+        <location filename="../outputqtmultimediafactory.cpp" line="34"/>
+        <source>Qt Multimedia Plugin</source>
+        <translation>Módulo Qt multimedios</translation>
+    </message>
+    <message>
+        <location filename="../outputqtmultimediafactory.cpp" line="59"/>
+        <source>About Qt Multimedia Output Plugin</source>
+        <translation>Acerca del Módulo Multimedios Qt</translation>
+    </message>
+    <message>
+        <location filename="../outputqtmultimediafactory.cpp" line="60"/>
+        <source>Qmmp Qt Multimedia Output Plugin</source>
+        <translation>Módulo de Salida de Multimedios Qt de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../outputqtmultimediafactory.cpp" line="61"/>
+        <source>Written by: Ivan Ponomarev &lt;ivantrue@gmail.com&gt;</source>
+        <translation>Escrito por: Ivan Ponomarev &lt;ivantrue@gmail.com&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Qt Multimedia Plugin Settings</source>
+        <translation>Configuración de Módulo de Salida de Multimedios Qt</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="46"/>
+        <source>Device:</source>
+        <translation>Dispositivo:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="36"/>
+        <source>Default</source>
+        <translation>Predeterminado</translation>
+    </message>
+</context>
+</TS>
---
+++ qmmp-1.1.12-qt5/src/plugins/Output/wasapi/translations/wasapi_plugin_es.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>OutputWASAPIFactory</name>
+    <message>
+        <location filename="../outputwasapifactory.cpp" line="29"/>
+        <source>WASAPI Plugin</source>
+        <translation>Módulo WASAPI</translation>
+    </message>
+    <message>
+        <location filename="../outputwasapifactory.cpp" line="53"/>
+        <source>About WASAPI Output Plugin</source>
+        <translation>Acerca del Módulo de Salida WASAPI</translation>
+    </message>
+    <message>
+        <location filename="../outputwasapifactory.cpp" line="54"/>
+        <source>Qmmp WASAPI Output Plugin</source>
+        <translation>Módulo de Salida WASAPI de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../outputwasapifactory.cpp" line="55"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Escrito por: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+</TS>
